59b8c8b9befc6a67b888902a6e7639eeb350aa92
Joseph Sungpil Choi <chousemath@gmail.com>
2017-07-17 13:08:16 +0900

    created a rudimentary repl

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/chousemath/monkey/lexer"
+	"github.com/chousemath/monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		// read from input source until a new line is encountered
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		// take the new line, create a new instance of a lexer, print all tokens
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
